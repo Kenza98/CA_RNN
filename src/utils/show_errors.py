5f09ab8565754c45e7bbb9b906ad4f4ac74bd454
Kenza Benjelloun <kenzab15@gmail.com>
2026-03-25 16:55:36 +0100

    .

--- src/utils/show_errors.py
+++ src/utils/show_errors.py
@@ -1,8 +1,32 @@
 import torch
 from pathlib import Path
+import re
 
 PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent
-OUT_DIR = PROJECT_ROOT / "outputs/shuffle_True_ep100"
+OUT_DIR = PROJECT_ROOT / "outputs" / "shuffle_True_ep100"
+
+def find_result_file(pattern_str):
+    pattern = re.compile(pattern_str)
+    files = [f for f in OUT_DIR.iterdir() if pattern.match(f.name)]
+    if not files:
+        raise FileNotFoundError(f"No file matching '{pattern_str}' in {OUT_DIR}")
+    return sorted(files, key=lambda f: f.stat().st_mtime)[-1]
+
+
+#loading error dicts for baseline
+bf=find_result_file(r".*baseline.*\.pt$") #baseline file
+lf=find_result_file(r".*lstm.*\.pt$") #baseline file
+gf=find_result_file(r".*gru.*\.pt$") #baseline file
+vf=find_result_file(r".*vrnn.*\.pt$") #baseline file
+
+print(f"The files :\n1. {bf}\n2. {lf}\n3. {gf}\n4. {vf}\n\n-----------------\n\n")
+exit(0)
+baseline     = torch.load(bf, weights_only=False)
+vanilla_rnn  = torch.load(vf, weights_only=False)
+lstm         = torch.load(lf, weights_only=False)
+gru          = torch.load(gf, weights_only=False)
+
+
 
 # loading error dicts for each model
 baseline = torch.load(OUT_DIR / "baselines.pt")  # file, not dict... yet?
